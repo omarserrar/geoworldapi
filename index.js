580a7ff90e463093f79dbbef7d882fd7a986caba
Omar Serrar <omarserrar2@gmail.com>
2025-09-01 17:04:17 +0200

    app ads.txt

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static("public")); 
 // ---- Small util to wrap async route handlers ----
 const asyncHandler = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
